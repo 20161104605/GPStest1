12af51b071cdf91da723750ec96a32a289714ae6
20161104605 <fq20161104605@admindeiMac-33.local>
2017-06-22 11:55:00 +0800

    GPS文件提取时间，纬度等数据
    GPStest1

--- GPStest1/main.c
+++ GPStest1/main.c
@@ -10,6 +10,26 @@
 
 int main(int argc, const char * argv[]) {
     // insert code here...
-    printf("Hello, World!\n");
+    FILE *fp;
+    char str1[70];
+    char str2[70];
+    char lat[9],time[7],minute[7];
+    int i;
+    fp=fopen("//Users//fq20161104605//Desktop//GPStest1//GPS170510.log", "r+");
+    fscanf(fp,"%s%s",str1,str2);
+    printf("结果：%s\n%s\n",str1,str2);
+    for(i=0;i<8;i++)
+        lat[i]=str1[i+16];
+    lat[8]='\0';
+    printf("纬度：%s\n",lat);
+    for(i=0;i<6;i++)
+        time[i]=str1[i+7];
+    time[6]='\0';
+    printf("时间：%s\n",time);
+    for(i=0;i<6;i++)
+        minute[i]=str1[i+51];
+    minute[6]='0';
+    printf("确切时间：%s\n",minute);
+    fclose(fp);
     return 0;
 }
