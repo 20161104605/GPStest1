d7fabb60b924073340ef06b74a82e3f00bda26b5
20161104605 <fq20161104605@admindeiMac-33.local>
2017-07-03 09:56:58 +0800

    完成
    最终完成，写入csv文件

--- GPStest1/main.c
+++ GPStest1/main.c
@@ -17,40 +17,60 @@ int main(int argc, const char * argv[]) {
     char moon[3],year[3],mouth[3],day[3];
     int i;
     fp=fopen("//Users//fq20161104605//Desktop//GPStest1//GPS170510.log", "r+");
-    fp1=fopen("//Users//fq20161104605//Desktop//GPStest1//GPSTXT","w+");
+    fp1=fopen("//Users//fq20161104605//Desktop//GPStest1//GPSTXT.csv","w");
+    
     while(fscanf(fp,"%s%s",str1,str2)!=EOF)
     {
     printf("结果：%s\n%s\n",str1,str2);
+        
     for(i=0;i<8;i++)
         lat[i]=str1[i+16];
     lat[8]='\0';
     printf("纬度：%s\n",lat);
-    fprintf(fp1,"纬度：%s\n",lat);
+    fprintf(fp1,"纬度：%s  ",lat);
+        
+        
+        
     for(i=0;i<6;i++)
         time[i]=str1[i+7];
     time[6]='\0';
     printf("时间：%s\n",time);
-    fprintf(fp1,"时间：%s\n",time);
+    fprintf(fp1,"时间：%s  ",time);
+        
+        
+        
     for(i=0;i<6;i++)
         minute[i]=str1[i+51];
     minute[6]='\0';
     printf("地磁偏角：%s\n",minute);
-    fprintf(fp1,"地磁偏角：%s\n",minute);
+    fprintf(fp1,"地磁偏角：%s  ",minute);
+        
+        
+        
     for(i=0;i<10;i++)
         longitude[i]=str1[i+27];
     longitude[9]='\0';
     printf("经度：%s\n",longitude);
-    fprintf(fp1,"经度：%s\n",longitude);
+    fprintf(fp1,"经度：%s  ",longitude);
+        
+        
+        
     for(i=0;i<5;i++)
         speed[i]=str1[i+39];
     speed[5]='\0';
     printf("地面速率：%s\n",speed);
-    fprintf(fp1,"地面速率：%s\n",speed);
+    fprintf(fp1,"地面速率：%s  ",speed);
+        
+        
+        
     for(i=0;i<5;i++)
         hight[i]=str2[i+43];
     hight[4]='\0';
     printf("海拔高度：%s\n",hight);
-    fprintf(fp1,"海拔高度：%s\n",hight);
+    fprintf(fp1,"海拔高度：%s  ",hight);
+        
+        
+        
     for(i=0;i<2;i++)
         hour[i]=str1[i+8];
     hour[1]='\0';
@@ -61,12 +81,18 @@ int main(int argc, const char * argv[]) {
         ss[i]=str1[i+11];
     ss[2]='\0';
     printf("UTC时间：%s时%s分%s秒\n",hour,mm,ss);
-        fprintf(fp1,"UTC时间：%s时%s分%s秒\n",hour,mm,ss);
+        fprintf(fp1,"UTC时间：%s时%s分%s秒",hour,mm,ss);
+        
+        
+        
         for(i=0;i<2;i++)
             moon[i]=str2[i+39];
         moon[2]='\0';
         printf("卫星：%s\n",moon);
-        fprintf(fp1,"卫星：%s\n",moon);
+        fprintf(fp1,"卫星：%s  ",moon);
+        
+        
+        
         for(i=0;i<2;i++)
             year[i]=str1[i+55];
         year[2]='\0';
@@ -77,9 +103,10 @@ int main(int argc, const char * argv[]) {
             day[i]=str1[i+51];
         day[2]='\0';
         printf("确切时间：20%s年%s月%s日\n",year,mouth,day);
-        fprintf(fp1,"确切时间：20%s年%月%s日\n",year,mouth,day);
+        fprintf(fp1,"确切时间：20%s年%s月%s日\n",year,mouth,day);
+    
     }
     fclose(fp);
-    
+    fclose(fp1);
     return 0;
 }
